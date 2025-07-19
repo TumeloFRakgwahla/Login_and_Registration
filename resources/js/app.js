c67f916495dc6d4acc304d6c71898dbd86a360a7
TumeloFRakgwahla <tumelorakgwahla@gmail.com>
2025-07-19 14:58:07 +0200

    intergrating vue into laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import vuetify from "./vuetify";
+import app from "./layouts/app.vue";
+ 
+createApp(app).use(vuetify).mount("#app");
