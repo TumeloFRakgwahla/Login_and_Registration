15b4783f5d65040f641c91d60cb989b39bd1917a
TumeloFRakgwahla <tumelorakgwahla@gmail.com>
2025-07-21 21:38:49 +0200

    Add Welcome.vue, login and Registration auth and a Dashboard

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+function handleImageError() {
+    document.getElementById('screenshot-container')?.classList.add('!hidden');
+    document.getElementById('docs-card')?.classList.add('!row-span-1');
+    document.getElementById('docs-card-content')?.classList.add('!flex-row');
+    document.getElementById('background')?.classList.add('!hidden');
+}
+</script>
+
+<template>
+   <Head title="Welcome" />
+
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm">
+            Dashboard</Link>
+
+            <template v-else>
+                <Link :href="route('login')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm">
+                Log in</Link>
+
+                <Link v-if="canRegister" :href="route('register')"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm">
+                Register</Link>
+            </template>
+        </div>
+
+        <main class="shadow-2xl bg-gray-600/50 rounded-lg place-content-center w-1/2 min-h-72">
+            <h1 
+                class="text-6xl font-mono font-extrabold text-white  text-center text-wrap">
+                Welcome To Kgwahla Trading and Projects
+            </h1>
+        </main>
+    </div>
+</template>
